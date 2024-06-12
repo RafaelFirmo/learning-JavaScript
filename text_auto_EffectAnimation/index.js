c41766aa80e406fd02ca4a9a4ab5ca1ab06d39bd
RafaelFirmo <93453022+RafaelFirmo@users.noreply.github.com>
2024-06-12 15:36:43 -0300

    Update index.js
    Adicionado texto de explicação.

--- text_auto_EffectAnimation/index.js
+++ text_auto_EffectAnimation/index.js
@@ -25,3 +25,28 @@ function updateText() {
     setTimeout(updateText, 300);
 };
 
+/*1 - const containerEl = document.querySelector(".conteiner");
+  - Aqui, um elemento HTML com a classe .conteiner será adicionadodocument.querySelector(".conteiner").
+  - O elemento selecionado será armazenado na variável containerEl.
+
+2 - const careers = ["World!", "Front end nation", "Back end nation."];
+  - Este é um array chamado careers que contém algumas profissões.
+
+3 - Duas variáveis são declaradas:
+  - careerIndex é inicializada com o valor zero.
+  - characterIndex também é inicializada com o valor zero.
+
+4 - updateText();
+  - Chamamos a função updateText() para iniciar o processo de atualização do texto.
+
+5 - function updateText() { ... }
+  - Aqui está a definição da função updateText().
+  - Dentro dessa função:
+    characterIndex é incrementado.
+    O conteúdo do elemento containerEl é atualizado com um título <h1> que exibe a profissão atual (com         base nos índices careerIndex e characterIndex).
+    Se characterIndex atingir o comprimento da profissão atual, incrementamos careerIndex e redefinimos characterIndex para zero.
+    Se careerIndex atingir o comprimento do array careers, reiniciamos careerIndex para zero.
+    A função updateText() é chamada novamente após um atraso de 300 milissegundos usando setTimeout.
+*/
+
+
